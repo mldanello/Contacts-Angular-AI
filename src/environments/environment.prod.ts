765f4dc4c12db7d09ce478cf2d650436a46bec20
Mark Danello <mdanello26@gmail.com>
2026-07-10 11:59:15 -0400

    Track environment files and update ignore rules

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  appVersion: 'v1.0.3',
+  apiBaseUrl: 'https://mdms-api-app-ggg5f9dxggfmefby.eastus-01.azurewebsites.net',
+  useMockBackend: false
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  appVersion: 'v1.0.3',
+  apiBaseUrl: 'http://localhost:4200/api',
+  useMockBackend: false
+};
