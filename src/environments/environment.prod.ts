8c23629ad82f4fe9cc594e777b35dc1d3dac0932
Mark Danello <mdanello26@gmail.com>
2026-07-10 15:35:08 -0400

    release: bump to v1.0.3.2 and deploy artifact

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appVersion: 'v1.0.3',
+  appVersion: 'v1.0.3.2',
   apiBaseUrl: 'https://mdms-api-app-ggg5f9dxggfmefby.eastus-01.azurewebsites.net',
   useMockBackend: false
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  appVersion: 'v1.0.3',
-  apiBaseUrl: 'http://localhost:4200/api',
-  useMockBackend: false
+	production: false,
+	appVersion: 'v1.0.3.2',
+	apiBaseUrl: 'http://localhost:4200/api',
+	useMockBackend: false
 };
