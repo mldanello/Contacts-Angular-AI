e7acfe7a874e3482dcd027cc4f75f2fb7faf6dc1
Mark Danello <mdanello26@gmail.com>
2026-08-11 16:50:57 -0400

    update version and deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appVersion: '1.0.5.0',
+  appVersion: '1.0.5.1',
   apiBaseUrl: 'https://mdms-api-app-ggg5f9dxggfmefby.eastus-01.azurewebsites.net',
   useMockBackend: false
 };
